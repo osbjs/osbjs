4d50fa109f17c50f663dbab9d7b39cf59598d55d
Nanachi <dinhvu2509@gmail.com>
2021-11-25 12:43:23 +0700

    add osb math dependency + osbvector can now initialize from vector2

--- src/Utils/OsbVector2.ts
+++ src/Utils/OsbVector2.ts
@@ -1,9 +1,15 @@
+import { Vector2 } from '@osbjs/math'
+
 export class OsbVector2 {
 	x: number
 	y: number
 	constructor(x: number, y: number) {
 		this.x = x
 		this.y = y
+	}
+
+	static fromVector2(v: Vector2) {
+		return new OsbVector2(v.x, v.y)
 	}
 
 	toString(): string {
