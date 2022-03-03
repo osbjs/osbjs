5b2ffc60874ba2243cfdeb93c65d84a8ecc0dcfc
Nanachi <dinhvu2509@gmail.com>
2022-03-03 09:08:31 +0700

    fix wiggle algo

--- src/Core/Utils/Expressions.ts
+++ src/Core/Utils/Expressions.ts
@@ -1,6 +1,6 @@
 import { Commandable } from '../Components'
 import { OsbColor, OsbVector2 } from '.'
-import { Vector2, noise2D } from '../../Math'
+import { Vector2, noise2D, lerp } from '../../Math'
 
 export function wiggleXY(
 	commandable: Commandable,
@@ -15,8 +15,8 @@ export function wiggleXY(
 	const timestep = 1000 / frequency
 
 	do {
-		const newX = amplitude * noise2D(x, t) + origin.x,
-			newY = amplitude * noise2D(y, t) + origin.y
+		const newX = amplitude * noise2D(origin.x, t) + origin.x - amplitude,
+			newY = amplitude * noise2D(origin.y, t) + origin.y - amplitude
 
 		commandable.Move(t, t + timestep, new OsbVector2(x, y), new OsbVector2(newX, newY))
 
@@ -32,7 +32,7 @@ export function wiggleX(commandable: Commandable, frequency: number, amplitude: 
 	const timestep = 1000 / frequency
 
 	do {
-		const newX = amplitude * noise2D(x, t) + originX
+		const newX = amplitude * noise2D(originX, t) + originX - amplitude
 
 		commandable.MoveX(t, t + timestep, x, newX)
 
@@ -47,7 +47,7 @@ export function wiggleY(commandable: Commandable, frequency: number, amplitude: 
 	const timestep = 1000 / frequency
 
 	do {
-		const newY = amplitude * noise2D(y, t) + originY
+		const newY = amplitude * noise2D(originY, t) + originY - amplitude
 
 		commandable.MoveY(t, t + timestep, y, newY)
 
@@ -69,7 +69,7 @@ export function wiggleRotation(
 	const timestep = 1000 / frequency
 
 	do {
-		const newAngle = amplitude * noise2D(angle, t) + originAngle
+		const newAngle = amplitude * noise2D(originAngle, t) + originAngle - amplitude
 
 		commandable.Rotate(t, t + timestep, angle, newAngle)
 
@@ -84,28 +84,32 @@ export function wiggleColor(
 	amplitude: number,
 	startTime: number,
 	endTime: number,
-	color1: OsbColor,
-	color2: OsbColor
+	minGradient: OsbColor,
+	maxGradient: OsbColor
 ) {
-	const originR = Math.round((color1.r + color2.r) / 2),
-		originG = Math.round((color1.g + color2.g) / 2),
-		originB = Math.round((color1.b + color2.b) / 2)
+	const originR = Math.round((minGradient.r + maxGradient.r) / 2),
+		originG = Math.round((minGradient.g + maxGradient.g) / 2),
+		originB = Math.round((minGradient.b + maxGradient.b) / 2)
+
+	const ampR = Math.min(Math.round(amplitude), originR),
+		ampG = Math.min(Math.round(amplitude), originR),
+		ampB = Math.min(Math.round(amplitude), originR)
 
 	let r = originR,
 		g = originG,
 		b = originB
 
-	const _ampR = Math.min(amplitude, originR),
-		_ampG = Math.min(amplitude, originG),
-		_ampB = Math.min(amplitude, originB)
-
 	let t = startTime
 	const timestep = 1000 / frequency
 
 	do {
-		const newR = Math.round(_ampR * noise2D(r, t)) + originR,
-			newG = Math.round(_ampG * noise2D(g, t)) + originG,
-			newB = Math.round(_ampB * noise2D(b, t)) + originB
+		const noiseR = noise2D(originR, t),
+			noiseG = noise2D(originG, t),
+			noiseB = noise2D(originB, t)
+
+		const newR = lerp(noiseR, -1, 1, originR - ampR, originR + ampR),
+			newG = lerp(noiseG, -1, 1, originG - ampG, originG + ampG),
+			newB = lerp(noiseB, -1, 1, originB - ampB, originB + ampB)
 
 		commandable.Color(t, t + timestep, new OsbColor(r, g, b), new OsbColor(newR, newG, newB))
 
@@ -129,7 +133,7 @@ export function wiggleOpacity(
 	const timestep = 1000 / frequency
 
 	do {
-		const newAngle = amplitude * noise2D(opacity, t) + originOpacity
+		const newAngle = amplitude * noise2D(originOpacity, t) + originOpacity - amplitude
 
 		commandable.Fade(t, t + timestep, opacity, newAngle)
 
