7d4aedfb69b128e5c9c5666bf48ed2b92eba70e8
Nanachi <dinhvu2509@gmail.com>
2022-03-18 00:09:38 +0700

    fix loop

--- src/Core/Commands/Loop.ts
+++ src/Core/Commands/Loop.ts
@@ -5,7 +5,7 @@ export class Loop extends CommandGroup {
 	count: number
 
 	constructor(startTime: number, count: number) {
-		super(` L,${startTime},${count}`)
+		super(` L,${startTime},${count}\n`)
 		this.startTime = startTime
 		this.count = count
 	}
--- src/Core/Commands/Trigger.ts
+++ src/Core/Commands/Trigger.ts
@@ -8,7 +8,7 @@ export class Trigger extends CommandGroup {
 	/**
 	 * for more info on `triggerName`, see https://osu.ppy.sh/wiki/en/Storyboard/Scripting/Compound_Commands */
 	constructor(triggerName: TriggerName | string, startTime: number, endTime: number) {
-		super(` T,${triggerName},${startTime},${endTime}`)
+		super(` T,${triggerName},${startTime},${endTime}\n`)
 
 		this.triggerName = triggerName
 		this.startTime = startTime
