d94010524b356386b7a5516256469663eeed4b56
Nanachi <dinhvu2509@gmail.com>
2024-10-05 00:27:41 +0700

    update

--- packages/core/src/parser/Background.ts
+++ packages/core/src/parser/Background.ts
@@ -8,16 +8,18 @@ export class Background {
   readonly path: string
   /** The position of the element. */
   readonly position: Vector2
-  /**
-   * @param options - The options for initializing the video.
-   * @param options.path - The path to the video's file relative to the beatmap folder.
-   * @param [options.position] - The position of the background image.
-   */
+
   constructor({
     path,
     position,
   }: {
+    /**
+     * The path to the video's file relative to the beatmap folder.
+     */
     path: string
+    /**
+     * The position of the background image.
+     */
     position: IVector2 | Vector2Tuple
   }) {
     this.path = path
